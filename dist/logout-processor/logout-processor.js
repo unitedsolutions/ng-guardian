0044e532ba086d36b010625d1be58572950bda30
daretobebetter <30729632+daretobebetter@users.noreply.github.com>
2018-10-26 13:51:01 -0400

    Add files via upload

--- dist/logout-processor/logout-processor.js
+++ dist/logout-processor/logout-processor.js
@@ -1,8 +1,24 @@
+import * as _ from "lodash";
 import autoLogoutSetter from '../auto-logout-setter/auto-logout-setter';
 import roleSetter from '../role-setter/role-setter';
 export default function () {
-    autoLogoutSetter('remove');
-    roleSetter.call(this, 'noAuth');
-    this.http.removeToken();
+    var _this = this;
+    return new Promise(function (resolve, reject) {
+        _this.http.get(_this.configs.logoutUrl).subscribe(function (data) {
+            _this.auth = data.auth;
+            _.extend(_this, { data: data });
+            // clean up
+            autoLogoutSetter('remove');
+            roleSetter.call(_this, 'noAuth');
+            _this.http.removeToken();
+            resolve(data);
+        }, function (err) {
+            // clean up
+            autoLogoutSetter('remove');
+            roleSetter.call(_this, 'noAuth');
+            _this.http.removeToken();
+            return reject(err);
+        });
+    });
 }
 //# sourceMappingURL=logout-processor.js.map
