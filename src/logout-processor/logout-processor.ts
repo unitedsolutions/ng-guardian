3c5dd9d02196a634925bbac464fda3e4ac77dec4
daretobebetter <30729632+daretobebetter@users.noreply.github.com>
2018-10-26 13:48:55 -0400

    Add files via upload

--- src/logout-processor/logout-processor.ts
+++ src/logout-processor/logout-processor.ts
@@ -1,8 +1,26 @@
+import * as _           from "lodash";
 import autoLogoutSetter from '../auto-logout-setter/auto-logout-setter';
 import roleSetter       from '../role-setter/role-setter';
 
 export default function() {
-  autoLogoutSetter('remove');
-  roleSetter.call(this, 'noAuth');
-  this.http.removeToken();
+  return new Promise((resolve, reject) => {
+    this.http.get(this.configs.logoutUrl).subscribe(
+      data => {
+        this.auth = data.auth;
+        _.extend(this, { data});
+        // clean up
+        autoLogoutSetter('remove');
+        roleSetter.call(this, 'noAuth');
+        this.http.removeToken();
+        resolve(data);
+      },
+      err => {
+        // clean up
+        autoLogoutSetter('remove');
+        roleSetter.call(this, 'noAuth');
+        this.http.removeToken();
+        return reject(err);
+      }
+    );
+  });
 }
